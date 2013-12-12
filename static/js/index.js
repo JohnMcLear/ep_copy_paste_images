069ee1688fa17b32212957b06d13cf9bc2cd0819
John McLear <john@mclear.co.uk>
2013-12-12 00:17:37 +0000

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,48 @@
+exports.aceAttribsToClasses = function(name, context){
+  if(context.key.indexOf("pastedImage:") !== -1){
+  var pastedImage =  /(?:^| )pastedImage:([^>]*)/.exec(context.key);
+    return ['pastedImage:' + pastedImage[1] ];
+  }
+  if(context.key == 'pastedImage'){
+    return ['pastedImage:' + context.value ];
+  }
+}
+
+exports.aceDomLineProcessLineAttributes = function(name, context){
+  var cls = context.cls;
+  var domline = context.domline;
+  var exp = /(?:^| )pastedImage:([^>]*)/;
+  var pastedImageType = exp.exec(cls);
+  if (pastedImageType[1]){
+    var modifier = {
+      preHtml: pastedImageType[1]+'>',
+      postHtml: '',
+      processedMarker: true
+    };
+    return [modifier];
+  }
+  return [];
+};
+
+exports.aceGetFilterStack = function(name, context){
+  return [
+    context.linestylefilter.getRegexpFilter(
+      new RegExp("pastedImage", "g"), 'pastedImage')
+  ];
+}
+
+exports.collectContentLineText = function(name, context){
+}
+
+exports.collectContentPre = function(name, context){
+}
+
+exports.collectContentPost = function(name, context){
+}
+
+exports.aceCreateDomLine = function(name, args){
+}
+
+exports.acePostWriteDomLineHTML = function (name, context){
+}
+
